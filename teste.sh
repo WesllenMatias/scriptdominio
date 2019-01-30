40ad44d7fb58b26513bacac2d6d0e67d8823e30e
Wesllen <wesllen.maia2010@gmail.com>
2019-01-29 21:26:54 -0300

    Adicionado o Shellbot e configurado o script teste.sh

--- teste.sh
+++ teste.sh
@@ -17,11 +17,14 @@
 # Testado em:
 #   bash 4.4.19
 # ------------------------------------------------------------------------ #
+# Importando API
+source ShellBot.sh
 
 # ------------------------------- VARIÁVEIS ----------------------------------------- #
-
+bot_token=' 625744069:AAGTXbrysLdWXsapmqhI9SPYpgledYJWPa4 '
 # ------------------------------------------------------------------------ #
-
+# Inicializando o bot
+ShellBot.init --token "$bot_token"
 # ------------------------------- TESTES ----------------------------------------- #
 [! -x $(wich lynx)]&& apt install lynx -y
 # ------------------------------------------------------------------------ #
@@ -34,7 +37,33 @@
 
 
 infos="$( lynx -source http://download.dominiosistemas.com.br/atualizacao/contabil/101b01/atualizacoes/ | grep C101 )"
-echo "$infos" | sed 's/<img.*exe">//;s/<.*\/a>//'
+#echo "$infos" | sed 's/<img.*exe">//;s/<.*\/a>//'
+# loop
+while :
+do
+    # Obtendo atualizações
+    ShellBot.getUpdates --limit 100 --offset $(ShellBot.OffsetNext) --timeout 30
 
+    # Lista os índices das atualizações.
+    for id in $(ShellBot.ListUpdates)
+    do
+    # Bloco de instruções
+    (
+        # Verifica se a mensagem é do tipo comando.
+        if [[ ${message_entities_type[$id]} == bot_command ]]
+        then
+            # Remove o sufixo contendo o username do bot e o '@' inclusive, extraindo somente o comando.
+            case ${message_text[$id]%%@*} in
+                '/dominio')
+                    # Envia mensagem ao remetente.
+                    ShellBot.sendMessage --chat_id ${message_chat_id[$id]} \
+                                         --text "$infos" | sed 's/<img.*exe">//;s/<.*\/a>//' \
+                                         --parse_mode markdown
+                ;;
+            esac
+         fi
+    ) & # Thread
+    done
+done
 
 # ------------------------------------------------------------------------ #
