c3b61c64d2fe5bbe1d00ce8d17c5da6faa5e63ed
WESLLEN MATIAS MAIA <46201054+WesllenMatias@users.noreply.github.com>
2019-03-15 00:20:03 -0300

    Update sefaz.sh

--- sefaz.sh
+++ sefaz.sh
@@ -20,7 +20,7 @@
 source ShellBot.sh
 
 #Informando bot que será usado
-bot_token='625744069:AAGTXbrysLdWXsapmqhI9SPYpgledYJWPa4'
+bot_token=''
 
 # Inicializando o bot
 ShellBot.init --token "$bot_token"
